af5b99c1e7c6d6657163cb4663c57f5e86117852
Sebastián Salazar <sebastian.salazar.solano@gmail.com>
2026-05-13 09:47:47 -0600

    feat(gsap): register scrolltrigger plugin

--- src/lib/gsap-config.ts
+++ src/lib/gsap-config.ts
@@ -1,8 +1,13 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 gsap.defaults({
   ease: "power2.inOut",
   duration: 0.8,
 });
 
-export { gsap };
+export { gsap, ScrollTrigger };
